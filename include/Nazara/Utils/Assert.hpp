dc320157d1c8c25df5194cdcda64afcd22dbb13c
Lynix <lynix680@gmail.com>
2022-05-22 23:39:12 +0200

    Assert: Add NAZARA_UTILS_ASSERTION_FAILURE

--- include/Nazara/Utils/Assert.hpp
+++ include/Nazara/Utils/Assert.hpp
@@ -8,10 +8,18 @@
 #define NAZARA_UTILS_ASSERT_HPP
 
 #include <Nazara/Prerequisites.hpp>
-#include <cassert>
+
+#ifndef NAZARA_UTILS_ASSERTION_FAILURE
+
+#include <cstdlib>
+#include <iostream>
+
+#define NAZARA_UTILS_ASSERTION_FAILURE(err) { std::cerr << err << std::endl; std::abort(); }
+
+#endif
 
 #if NAZARA_UTILS_ENABLE_ASSERTS || defined(NAZARA_DEBUG)
-	#define NazaraAssert(a, err) if (!(a)) assert(!err);
+	#define NazaraAssert(a, err) if (!(a)) NAZARA_UTILS_ASSERTION_FAILURE(err)
 #else
 	#define NazaraAssert(a, err) for (;;) break
 #endif
