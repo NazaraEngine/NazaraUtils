176003dadb5690b52bf7c24bfc0740a3df57af7f
SirLynix <lynix680@gmail.com>
2022-05-26 19:07:00 +0200

    Endianness: Add detection macros
    from https://sourceforge.net/p/predef/wiki/Endianness/

--- include/Nazara/Utils/Endianness.hpp
+++ include/Nazara/Utils/Endianness.hpp
@@ -13,10 +13,13 @@
 #if !defined(NAZARA_BIG_ENDIAN) && !defined(NAZARA_LITTLE_ENDIAN)
 	// Automatic detection following macros of compiler
 	#if defined(__m68k__) || defined(mc68000) || defined(_M_M68K) || (defined(__MIPS__) && defined(__MISPEB__)) || \
-		defined(__ppc__) || defined(__POWERPC__) || defined(_M_PPC) || defined(__sparc__) || defined(__hppa__)
+	    defined(__ppc__) || defined(__POWERPC__) || defined(_M_PPC) || defined(__sparc__) || defined(__hppa__) || \
+	    defined(__BIG_ENDIAN__) || defined(__ARMEB__) || defined(__THUMBEB__) || defined(__AARCH64EB__) || defined(_MIPSEB) || \
+	    defined(__MIPSEB) || defined(__MIPSEB__)
 		#define NAZARA_BIG_ENDIAN
 	#elif defined(__i386__) || defined(__i386) || defined(__X86__) || defined (__x86_64) || defined(_M_I86) || \
-		defined(_M_IX86) || defined(_M_X64)
+	    defined(_M_IX86) || defined(_M_X64) || defined(__LITTLE_ENDIAN__) || defined(__ARMEL__) || defined(__THUMBEL__) || \
+	    defined(__AARCH64EL__) || defined(_MIPSEL) || defined(__MIPSEL) || defined(__MIPSEL__)
 		#define NAZARA_LITTLE_ENDIAN
 	#else
 		#error Failed to identify endianness, you must define either NAZARA_BIG_ENDIAN or NAZARA_LITTLE_ENDIAN
