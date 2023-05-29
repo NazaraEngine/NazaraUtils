1e63b273b637fe8e72d7e818d5983e5e6c13e1eb
SirLynix <lynix680@gmail.com>
2023-05-29 17:11:47 +0200

    Add Tau constant

--- include/NazaraUtils/Algorithm.hpp
+++ include/NazaraUtils/Algorithm.hpp
@@ -27,6 +27,7 @@ namespace Nz
 	template<typename T> constexpr T Sqrt2  = T(1.4142135623730950488016887242097);
 	template<typename T> constexpr T Sqrt3  = T(1.7320508075688772935274463415059);
 	template<typename T> constexpr T Sqrt5  = T(2.2360679774997896964091736687313);
+	template<typename T> constexpr T Tau    = T(6.2831853071795864769252867665590);
 
 
 	// Math utils
