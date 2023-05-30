4658fcf282c31e148825c407068ac22c6f411da3
SirLynix <lynix680@gmail.com>
2023-05-30 17:54:47 +0200

    Make EnumValueCount work with enums using E::Count

--- include/NazaraUtils/Algorithm.hpp
+++ include/NazaraUtils/Algorithm.hpp
@@ -103,14 +103,30 @@ namespace Nz
 	template<typename T>
 	struct AlwaysFalse : std::false_type {};
 
+	template<typename T>
+	struct LazyVoid
+	{
+		using type = void;
+	};
+
+	template<typename T> using LazyVoid_t = typename LazyVoid<T>::type;
+
 	template<typename E, typename = void>
 	struct EnumValueCount
 	{
-		static_assert(AlwaysFalse<E>(), "enum has no Max field");
+		static_assert(AlwaysFalse<E>(), "enum has no Count nor Max field");
 	};
 
 	template<typename E>
-	struct EnumValueCount<E, std::void_t<decltype(E::Max)>>
+	struct EnumValueCount<E, LazyVoid_t<decltype(E::Count)>>
+	{
+		static_assert(std::is_enum_v<E>, "Type must be an enumeration");
+
+		static constexpr std::size_t value = static_cast<std::size_t>(E::Count);
+	};
+
+	template<typename E>
+	struct EnumValueCount<E, LazyVoid_t<decltype(E::Max)>>
 	{
 		static_assert(std::is_enum_v<E>, "Type must be an enumeration");
 
