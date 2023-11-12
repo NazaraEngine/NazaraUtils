2a789dab06da330398c1f49be1dff0e112aa2b0d
SirLynix <lynix680@gmail.com>
2023-11-12 11:01:08 +0100

    Replace deprecated asan mode by sanitizers options

--- tests/xmake.lua
+++ tests/xmake.lua
@@ -2,14 +2,14 @@ option("tests", { default = false, description = "Enable unit tests"})
 option("tests_cxx_version", { default = "cxx17", values = {"cxx17", "cxx20", "cxxlatest"}, description = "C++ version of unit tests" })
 
 if has_config("tests") then
-	if is_mode("asan") then
+	if has_config("asan") then
 		add_defines("CATCH_CONFIG_NO_WINDOWS_SEH")
 		add_defines("CATCH_CONFIG_NO_POSIX_SIGNALS")
 	end
 
 	add_requires("catch2 3")
 
-    target("UnitTests")
+    target("UnitTests", function ()
         set_kind("binary")
         set_group("Tests")
         add_headerfiles("src/**.hpp")
@@ -19,4 +19,5 @@ if has_config("tests") then
 
 		add_deps("NazaraUtils")
         add_packages("catch2")
+	end)
 end
--- xmake.lua
+++ xmake.lua
@@ -1,7 +1,22 @@
 set_project("NazaraUtils")
 
-add_rules("mode.asan", "mode.coverage", "mode.debug", "mode.releasedbg", "mode.release")
+add_rules("mode.coverage", "mode.debug", "mode.releasedbg", "mode.release")
 add_rules("plugin.vsxmake.autoupdate")
+
+-- Sanitizers
+local sanitizers = {
+	asan = "address",
+	lsan = "leak",
+	tsan = "thread",
+}
+
+for opt, policy in table.orderpairs(sanitizers) do
+	option(opt, { description = "Enable " .. opt, default = false })
+
+	if has_config(opt) then
+		set_policy("build.sanitizer." .. policy, true)
+	end
+end
 
 add_includedirs("include")
 set_languages("c89", "cxx17")
@@ -13,11 +28,12 @@ if is_mode("debug") then
     add_defines("NAZARA_DEBUG")
 end
 
-target("NazaraUtils")
+target("NazaraUtils", function ()
     set_kind("headeronly")
     set_group("Library")
     add_headerfiles("include/(NazaraUtils/**.hpp)")
     add_headerfiles("include/(NazaraUtils/**.inl)")
+end)
 
 includes("bench/*.lua")
 includes("tests/*.lua")
