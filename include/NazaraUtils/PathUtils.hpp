4226e712a396e0a5eb1be691fcd4bb8676b1fcdd
SirLynix <lynix680@gmail.com>
2024-01-13 14:26:26 +0100

    PathUtils: Fix [[nodiscard]] and inline keyword order

--- include/NazaraUtils/PathUtils.hpp
+++ include/NazaraUtils/PathUtils.hpp
@@ -15,8 +15,8 @@
 namespace Nz
 {
 	// Path utils
-	inline [[nodiscard]] std::string PathToString(const std::filesystem::path& path);
-	inline [[nodiscard]] std::filesystem::path Utf8Path(std::string_view path);
+	[[nodiscard]] inline std::string PathToString(const std::filesystem::path& path);
+	[[nodiscard]] inline std::filesystem::path Utf8Path(std::string_view path);
 }
 
 #include <NazaraUtils/PathUtils.inl>
