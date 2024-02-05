0b11983e88cbef9c07230bd90fb99cb6fef00ed8
SirLynix <lynix680@gmail.com>
2024-02-05 13:29:16 +0100

    Fix warning

--- tests/src/HashTests.cpp
+++ tests/src/HashTests.cpp
@@ -19,27 +19,27 @@ void TestHash(const char (&str)[N], H expectedHash)
 
 TEST_CASE("Hash", "[Hash]")
 {
-	static_assert(Nz::CRC32("Nazara Engine") == 0x8a2f5235);
-	static_assert(Nz::CRC32("t.tv/SirLynixVanFriejtes") == 0xcc2a0914);
-	static_assert(Nz::CRC32("The quick brown fox jumps over the lazy dog") == 0x414fa339);
+	static_assert(Nz::CRC32("Nazara Engine") == 0x8a2f5235u);
+	static_assert(Nz::CRC32("t.tv/SirLynixVanFriejtes") == 0xcc2a0914u);
+	static_assert(Nz::CRC32("The quick brown fox jumps over the lazy dog") == 0x414fa339u);
 
-	TestHash<Nz::CRC32Hash>("Nazara Engine", 0x8a2f5235);
-	TestHash<Nz::CRC32Hash>("t.tv/SirLynixVanFriejtes", 0xcc2a0914);
-	TestHash<Nz::CRC32Hash>("The quick brown fox jumps over the lazy dog", 0x414fa339);
+	TestHash<Nz::CRC32Hash>("Nazara Engine", 0x8a2f5235u);
+	TestHash<Nz::CRC32Hash>("t.tv/SirLynixVanFriejtes", 0xcc2a0914u);
+	TestHash<Nz::CRC32Hash>("The quick brown fox jumps over the lazy dog", 0x414fa339u);
 
-	static_assert(Nz::FNV1a32("Nazara Engine") == 0x5ba735a6);
-	static_assert(Nz::FNV1a32("t.tv/SirLynixVanFriejtes") == 0x3ef9d843);
-	static_assert(Nz::FNV1a32("The quick brown fox jumps over the lazy dog") == 0x048fff90);
+	static_assert(Nz::FNV1a32("Nazara Engine") == 0x5ba735a6u);
+	static_assert(Nz::FNV1a32("t.tv/SirLynixVanFriejtes") == 0x3ef9d843u);
+	static_assert(Nz::FNV1a32("The quick brown fox jumps over the lazy dog") == 0x048fff90u);
 
-	TestHash<Nz::FNV1a32Hash>("Nazara Engine", 0x5ba735a6);
-	TestHash<Nz::FNV1a32Hash>("t.tv/SirLynixVanFriejtes", 0x3ef9d843);
-	TestHash<Nz::FNV1a32Hash>("The quick brown fox jumps over the lazy dog", 0x048fff90);
+	TestHash<Nz::FNV1a32Hash>("Nazara Engine", 0x5ba735a6u);
+	TestHash<Nz::FNV1a32Hash>("t.tv/SirLynixVanFriejtes", 0x3ef9d843u);
+	TestHash<Nz::FNV1a32Hash>("The quick brown fox jumps over the lazy dog", 0x048fff90u);
 
-	static_assert(Nz::FNV1a64("Nazara Engine") == 0xa00fb3557d90f6e6);
-	static_assert(Nz::FNV1a64("t.tv/SirLynixVanFriejtes") == 0x4d2631a6429ff643);
-	static_assert(Nz::FNV1a64("The quick brown fox jumps over the lazy dog") == 0xf3f9b7f5e7e47110);
+	static_assert(Nz::FNV1a64("Nazara Engine") == 0xa00fb3557d90f6e6u);
+	static_assert(Nz::FNV1a64("t.tv/SirLynixVanFriejtes") == 0x4d2631a6429ff643u);
+	static_assert(Nz::FNV1a64("The quick brown fox jumps over the lazy dog") == 0xf3f9b7f5e7e47110u);
 
-	TestHash<Nz::FNV1a64Hash>("Nazara Engine", 0xa00fb3557d90f6e6);
-	TestHash<Nz::FNV1a64Hash>("t.tv/SirLynixVanFriejtes", 0x4d2631a6429ff643);
-	TestHash<Nz::FNV1a64Hash>("The quick brown fox jumps over the lazy dog", 0xf3f9b7f5e7e47110);
+	TestHash<Nz::FNV1a64Hash>("Nazara Engine", 0xa00fb3557d90f6e6u);
+	TestHash<Nz::FNV1a64Hash>("t.tv/SirLynixVanFriejtes", 0x4d2631a6429ff643u);
+	TestHash<Nz::FNV1a64Hash>("The quick brown fox jumps over the lazy dog", 0xf3f9b7f5e7e47110u);
 }
